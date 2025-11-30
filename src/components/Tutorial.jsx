39e6b82d35b06e219a3c7d788e779e5be18d71a4
ChackBR <chacalgo@gmail.com>
2025-11-30 16:00:09 -0300

    Update Tutorial, fix typo

--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -12,7 +12,10 @@ const Tutorial = () => {
                         <div className="step-icon">1</div>
                         <h3>Dados de Acesso</h3>
                         <p>Tela de Login !</p>
-                        <li>Senha: 123</li>
+                        <ul className="feature-list">
+                           <li>Usuario: IMATION</li>
+                           <li>Senha: 123</li>
+                        </ul>
                         <div className="product-image">
                            <img src={`${import.meta.env.BASE_URL}Tela_Login.png`} alt="Software Screenshot" className="product-screenshot" />
                         </div>                        
@@ -21,8 +24,10 @@ const Tutorial = () => {
                         <div className="step-icon">2</div>
                         <h3>Dados do Emitente</h3>
                         <p>Cadastro de Empresas:</p>
-                        <li>Atualizar com os dados de sua empresa</li>
-                        <li>* Tela de exemplo *</li>
+                        <ul className="feature-list">
+                           <li>Atualizar com os dados de sua empresa</li>
+                           <li>* Tela de exemplo *</li>
+                        </ul>
                         <li></li>
                         <div className="product-image">
                            <img src={`${import.meta.env.BASE_URL}Tela_Empresas.jpg`} alt="Software Screenshot" className="product-screenshot" />
@@ -32,7 +37,9 @@ const Tutorial = () => {
                         <div className="step-icon">3</div>
                         <h3>Certificado digital</h3>
                         <p>Parâmetros: Certificado</p>
-                        <li>Caminho arquivo.pfx ou Número de Série</li>
+                        <ul className="feature-list">
+                           <li>Caminho arquivo.pfx ou Número de Série</li>
+                        </ul>
                         <li></li>
                         <div className="product-image">
                             <img src={`${import.meta.env.BASE_URL}Tela_Certificado.png`} alt="Software Screenshot" className="product-screenshot" />
@@ -42,8 +49,9 @@ const Tutorial = () => {
                         <div className="step-icon">4</div>
                         <h3>Documento Auxiar</h3>
                         <p>Parâmetros: DANFSe</p>
-                        <li>Ajuste os dados/imagens, conforme exemplo</li>
-                        <li></li>
+                        <ul className="feature-list">
+                           <li>Ajuste os dados/imagens, conforme exemplo</li>
+                        </ul>
                         <div className="product-image">
                             <img src={`${import.meta.env.BASE_URL}Tela_Danfse.jpg`} alt="Software Screenshot" className="product-screenshot" />
                         </div>                        
@@ -53,15 +61,15 @@ const Tutorial = () => {
                         <h3>Emissão: NFSe</h3>
                         <p></p>
                         <p>Vamos agora a emissão de Notas Fiscais: NFSe</p>
-                        <p></p>
-                        <li>Abaixo e a esquerda informe a Numeração já utilizada</li>
-                        <li>Clicar: Importar XML</li>
-                        <li></li>
-                        <li>Todas as NFSe já emitidas serão importadas para a base de dados</li>
-                        <li></li>
-                        <li>[➕] Nova NFSe:</li>
-                        <li>Preencha os campos vazios e clique em Enviar</li>
-                        <li></li>
+                        <ul className="feature-list">
+                           <li>Abaixo e a esquerda informe a Numeração já utilizada</li>
+                           <li>Clicar: Importar XML</li>
+                           <li></li>
+                           <li>Todas as NFSe já emitidas serão importadas para a base de dados</li>
+                           <li></li>
+                           <li>[➕] Nova NFSe:</li>
+                           <li>Preencha os campos vazios e clique em Enviar</li>
+                        </ul>
                         <p>[✅] Pronto para a próxima [✅]</p>
                         <div className="product-image">
                             <img src={`${import.meta.env.BASE_URL}Tela_NFSe.jpg`} alt="Software Screenshot" className="product-screenshot" />
