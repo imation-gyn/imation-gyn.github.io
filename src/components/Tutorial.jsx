ce326507bfaa1b3757304d5af8452bfa562e5bda
ChackBR <chacalgo@gmail.com>
2025-11-30 10:19:28 -0300

    Update Tutorial, fix typo

--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -5,8 +5,8 @@ const Tutorial = () => {
         <section id="tutorial" className="section tutorial">
             <div className="container">
                 <h2 className="section-title">Tutorial para Uso</h2>
-                <h3 className="section-title">Utilize as imagens exemplo e faça sua configuração do sistema</h3>
-                <h3 className="section-title">Informe os dados de sua(s) empresa(s)</h3>
+                <li className="section-title">Utilize as imagens exemplo e faça sua configuração do sistema</li>
+                <li className="section-title">Informe os dados de sua(s) empresa(s)</li>
                 <div className="tutorial-content">
                     <div className="step-card">
                         <div className="step-icon">1</div>
@@ -23,6 +23,7 @@ const Tutorial = () => {
                         <p>Cadastro de Empresas:</p>
                         <li>Atualizar com os dados de sua empresa</li>
                         <li>* Tela de exemplo *</li>
+                        <li></li>
                         <div className="product-image">
                            <img src={`${import.meta.env.BASE_URL}Tela_Empresas.jpg`} alt="Software Screenshot" className="product-screenshot" />
                         </div>                        
@@ -32,6 +33,7 @@ const Tutorial = () => {
                         <h3>Certificado digital</h3>
                         <p>Parâmetros: Certificado</p>
                         <li>Caminho arquivo.pfx ou Número de Série</li>
+                        <li></li>
                         <div className="product-image">
                             <img src={`${import.meta.env.BASE_URL}Tela_Certificado.png`} alt="Software Screenshot" className="product-screenshot" />
                         </div>                        
@@ -41,6 +43,7 @@ const Tutorial = () => {
                         <h3>Documento Auxiar</h3>
                         <p>Parâmetros: DANFSe</p>
                         <li>Ajuste os dados/imagens, conforme exemplo</li>
+                        <li></li>
                         <div className="product-image">
                             <img src={`${import.meta.env.BASE_URL}Tela_Danfse.jpg`} alt="Software Screenshot" className="product-screenshot" />
                         </div>                        
@@ -49,16 +52,16 @@ const Tutorial = () => {
                         <div className="step-icon">5</div>
                         <h3>Emissão: NFSe</h3>
                         <p></p>
-                        <p>Vamos agora a emissão de Notas Fiscais: NFSe</p>
+                        <p>Vamos agora a emissão de Notas Fiscais: NFSe</li>
                         <p></p>
-                        <p>Abaixo e a esquerda informe a Numeração já utilizada</p>
-                        <p>Clicar: Importar XML</p>
-                        <p></p>
-                        <p>Todas as NFSe já emitidas serão importadas para a base de dados</p>
-                        <p></p>
-                        <p>[➕] Nova NFSe:</p>
-                        <p>Preencha os campos vazios e clique em Enviar</p>
-                        <p></p>
+                        <li>Abaixo e a esquerda informe a Numeração já utilizada</p>
+                        <li>Clicar: Importar XML</li>
+                        <li></p>
+                        <li>Todas as NFSe já emitidas serão importadas para a base de dados</li>
+                        <li></li>
+                        <li>[➕] Nova NFSe:</li>
+                        <li>Preencha os campos vazios e clique em Enviar</li>
+                        <li></li>
                         <p>[✅] Pronto para a próxima [✅]</p>
                         <div className="product-image">
                             <img src={`${import.meta.env.BASE_URL}Tela_NFSe.jpg`} alt="Software Screenshot" className="product-screenshot" />
