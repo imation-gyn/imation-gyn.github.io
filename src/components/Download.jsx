c4501ea22bf4b5cff4108e957c74a9c62a1d42c0
ChackBR <chacalgo@gmail.com>
2025-12-17 22:38:32 -0300

    Update Version, 1.1.1.18

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -7,11 +7,11 @@ const Download = () => {
                 <h2>OpenErp NFSe - Grátis</h2>
                 <p>Rápido e Fácil de usar</p>
                 <div className="download-options">
-                    <a href="https://github.com/imation-gyn/imation-gyn.github.io/releases/download/OpenErp/OpenErp-1.1.1.17-x86-Instalador.exe" className="btn-download-large">
+                    <a href="https://github.com/imation-gyn/imation-gyn.github.io/releases/download/OpenErp/OpenErp-1.1.1.18-x86-Instalador.exe" className="btn-download-large">
                         <span className="icon">⬇️</span>
                         <span className="text">
                             <span className="label">Download para Windows</span>
-                            <span className="version">Versão 1.1.1.17 (x32/x64-bit)</span>
+                            <span className="version">Versão 1.1.1.18 (x32/x64-bit)</span>
                         </span>
                     </a>
                 </div>
